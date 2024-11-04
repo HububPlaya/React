8b8b6de38d072ac2b5d02a348ff0d35dc4da9a45
HububPlaya <keelan.moore14@gmail.com>
2024-11-04 00:20:38 -0600

    learned to pass a style object to an in line style

--- product-list/src/components/Goods.jsx
+++ product-list/src/components/Goods.jsx
@@ -1,7 +1,16 @@
 
 const Goods = ({products}) => {
+    const ProductsStyle = {
+        width: "300px",
+        padding: "10px",
+        borderRadius: "10px",
+        textAlign: "center",
+        fontSize: "24px",
+        fontWeight: "bold",
+        backgroundColor: "lightblue"
+    }
     return (
-        <ul>
+        <ul style={ProductsStyle}>
             {products.map(good => (
                 <li key={good.id}>
                 <h2>{good.name}</h2>
